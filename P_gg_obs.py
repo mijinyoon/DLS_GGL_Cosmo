5031599c0886ba2f242c01852b19270e51bbee09
mijinyoon <mijin@umich.edu>
2017-07-05 17:36:50 +0900

    Add files via upload

---
+++ P_gg_obs.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.special import jn
+from scipy.integrate import simps
+
+
+def f(x):
+    return x*jn(1,x)
+
+def P_gg_band(theta, w, lmax,lmin):
+    
+    delta_l = np.log(lmax/lmin)
+
+    return 2*np.pi/delta_l*simps(w*f(lmin*theta)-f(lmax*theta),np.log(theta))
---
+++ P_gm_obs.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy.special import jn
+from scipy.integrate import simps
+
+def h(x):
+    return -x*jn(1,x) - 2.*jn(0,x)
+
+def P_gm_band(theta, shear, lmax,lmin):
+    
+    delta_l = np.log(lmax/lmin)
+    
+    return 2*np.pi/delta_l*simps(shear*h(lmin*theta)-h(lmax*theta),np.log(theta))
